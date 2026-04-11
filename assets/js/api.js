765dcc76a02eea62a8d52c2dacc6291fc41eae23
James Johny <jamesjohny.sg@gmail.com>
2026-04-11 22:43:35 +0000

    Atualização de urls

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,5 @@
 async function fetchProfileData() {
-    /*const url = 'https://raw.githubusercontent.com/James-Johny/js-developer-portfolio/refs/heads/projeto-base/data/profile.json';*/
-    const url = 'https://potential-space-telegram-4jwqqj6wqj4x3qw5g-5500.app.github.dev/data/profile.json';
+    const url = 'https://github.com/James-Johny/js-developer-portfolio/raw/refs/heads/projeto-base/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 }
